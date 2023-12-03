8a168a5d87c433949532ad04a89652ea32204a2a
Wisest_wizard <12suhangp34@gmail.com>
2023-12-03 09:08:44 +0530

    Try to avoid `array initializer must be an initializer list or string literal`

--- src/version.c
+++ src/version.c
@@ -1,4 +1,7 @@
-static const char version[] = 4.16;
+#define VERSION_NUMBER 4.16
+#define VERSION_STRINGIFY_HELPER(x) #x
+#define VERSION_STRINGIFY(x) VERSION_STRINGIFY_HELPER(x)
+static const char version[] = VERSION_STRINGIFY(VERSION_NUMBER);
 const char *proxychains_get_version(void) {
 	return version;
 }
